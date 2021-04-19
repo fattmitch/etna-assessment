ee57ffd1e1792714c1abc1a116bebc1ad0dddce4
fattmitch <matt.s.fitch@gmail.com>
2021-04-19 11:20:16 -0700

    removing a test console log

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const mobileMenu = document.querySelector('.primary-navigation');
-console.log(menuToggle);
 
 function toggleMenu() {
   mobileMenu.classList.toggle('show');
