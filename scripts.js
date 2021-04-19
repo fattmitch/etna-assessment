63b6b104dceda3a88f73ef8f91c4254f4b1e635e
fattmitch <matt.s.fitch@gmail.com>
2021-04-19 11:19:35 -0700

    removing some old commented JS

--- scripts.js
+++ scripts.js
@@ -4,11 +4,6 @@ console.log(menuToggle);
 
 function toggleMenu() {
   mobileMenu.classList.toggle('show');
-  // if (mobileMenu.style.display === "block") {
-  //   mobileMenu.style.display = "none";
-  // } else {
-  //   mobileMenu.style.display = "block";
-  // }
 }
 
 menuToggle.addEventListener('click', toggleMenu);
